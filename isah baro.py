4a10aa5cdbdd23875269ddc66ea3e53ef4347a3d
Baro51 <81834378+Baro51@users.noreply.github.com>
2021-04-03 01:54:23 +0100

    Add files via upload

---
+++ isah baro.py
@@ -0,0 +1,25 @@
+print("NAME:Aliu Tunde Uthman")
+print("DEPARTMENT: Computer Science")
+scare = "Boo!"
+print (scare)
+var1 = 56
+var2 = 67
+third_score= var1 + var2
+print(var1, var2, third_score)
+var5 = 12
+var4 = .5
+var6 = var5 / var4
+print (var6)
+one = 34
+two = 56
+three = one + two
+print (three)
+dash = 8
+crash = dash % 3
+print (crash)
+var1 = 2
+var2 = var1 * 4
+var3 = 4
+var4 = var3 + 2
+print (var2 * var3 + 2)
+
